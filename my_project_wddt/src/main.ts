a53bf1cd4e624958335d908520931cfff32220aa
Whazt <walddt05@gmail.com>
2025-10-08 20:50:56 -0600

    Commit Final Guía #3

--- my_project_wddt/src/main.ts
+++ my_project_wddt/src/main.ts
@@ -10,11 +10,19 @@ async function bootstrap() {
     .setDescription('API description')
     .setVersion('1.0')
     .addTag('api')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      in: 'header',
+      name: 'Authorization',
+      description: 'Enter your Bearer Token'
+    })
+    .addSecurityRequirements('bearer')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
